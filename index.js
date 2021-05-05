655106622cdfdc080e2b8b3215680fe6e7cc8745
ryozdemir <80812621+ryozdemir@users.noreply.github.com>
2021-05-05 14:34:52 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function nextSong() {
 
 
 function updateProgress(e) {
-    const {duration, currentTime} = e.srcElemet
+    const {duration, currentTime} = e.srcElement
     const progressPercent = (currentTime / duration) * 100
     progress.style.width = `${progressPercent}%`
 }
